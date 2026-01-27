4c72b3a6b649330c8990a73e9286cc73e94f42da
Calle Stålhem <carl@stalhem.se>
2026-01-27 09:41:48 +0100

    Added custom alias file for Oh My Zsh

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,21 @@
+# --------------------------------
+#   Custom aliases
+# --------------------------------
+
+# --------------------------------
+#   ls
+
+alias ls='lsd -a'
+
+
+{{- if eq .chezmoi.hostname "m1-mbp" }}
+# --------------------------------
+#   macOS specific
+
+alias icloud='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/' # icloud goes to iCloud Drive
+{{- end }}
+
+# --------------------------------
+#   Executables
+
+alias edit='$EDITOR'              # Edit files with the current editor
